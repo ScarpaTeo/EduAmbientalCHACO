f2f7c33ff3d80e5c9613b05e9c6ef20d67cd2cb2
Teo Emmanuel Scarpa <46579699+ScarpaTeo@users.noreply.github.com>
2019-11-23 16:51:49 -0300

    ahora se pueden ver los pdf y las imagenes de la galeria tienen ventana modal

--- educacion-ambiental-chaco/src/Componentes/DetalleGaleri.js
+++ educacion-ambiental-chaco/src/Componentes/DetalleGaleri.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import imgpdf from '../Componentes/Img/pdf.png'
 
 
 
@@ -7,16 +8,77 @@ const DetalleGaleri = ({galeria}) => {
     const {name} = galeria;
 
     let newImg2='https://localhost:44398/images/'+name;
+    let target ='#'+name
+    let idtarget =name
+
+    let mostrar
+    if (name.includes('.pdf')){
+        mostrar=<div className="row">
+                    <div className=".col-md-4">
+                        <div className="card mx-auto">
+                            <div className="card-body">
+                                <p className="text-center">{name}</p>
+                                <img src={imgpdf} style={{borderRadius:"5px",padding:"20px"}} width="300px" alt="Card image cap"/>
+                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target={target}> ver
+                                </button>
+
+                            <div class="modal fade" id={idtarget} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                            aria-hidden="true">
+                            <div class="modal-dialog modal-lg" role="document">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">{name}</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                <object data={newImg2} type='pdf/html' width="100%" height="500" style={{height: '85vh'}}/>
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                            </div>
+                        </div>            
+                    </div>
+                </div>
+    }else{
+        mostrar=<div className="row">
+                    <div className="col-md-4">
+                    <div className="card mx-auto" style={{width:'20rem', margin:'30px'}}>
+                        <div className="card-body">
+                        <img src={newImg2} style={{borderRadius:"5px",padding:"20px"}} width="300px" alt="Card image cap"/>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target={target}> ver
+                        </button>
+
+                            <div class="modal fade" id={idtarget} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                            aria-hidden="true">
+                            <div class="modal-dialog modal-lg" role="document">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">{name}</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <img src={newImg2} width="100%" alt="#"/>
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    </div>
+                </div>
+    }
 
 
 
   return (
         <React.Fragment>
-            <div className="row">
-                    <div className="card" style={{width:'20rem', margin:'30px'}}>
-                    <img src={newImg2} style={{borderRadius:"5px",padding:"20px"}} width="300px" alt="Card image cap"/>
-                    </div>
-            </div>
+            {mostrar}
 
         </React.Fragment>
     )
