dd3acd049b766a57681131e8136a1c2c4d87428c
Teo Emmanuel Scarpa <46579699+ScarpaTeo@users.noreply.github.com>
2019-11-26 03:55:17 -0300

    se mejoro el formulario de contacto

--- educacion-ambiental-chaco/src/Componentes/FormularioContact.js
+++ educacion-ambiental-chaco/src/Componentes/FormularioContact.js
@@ -10,17 +10,19 @@ const FormularioCont = () => {
   return (
         <React.Fragment>
             <div className="px-3 py-3">
-            <img className="img-fluid animated bounceInDown slower" src={logo}  width="300"/>
-                
+                <img className="img-fluid animated bounceInDown slower" src={logo}  width="300"/>
             </div>
-            <section class="mb-4 card mx-2">
+            <div className=" card mx-auto animated bounceInLeft delay-1 slower" style={{width:'50rem', margin:'30px', borderRadius:"35px"}}>
 
-                <h2 class="h1-responsive font-weight-bold text-center my-4">Contactanos y Sumate</h2>
-                
+                <br/>
+                <br/>
+                <h2 class="h1-responsive font-weight-bold text-center my-4 blue-text">Contactanos y Sumate</h2>
+                <br/>
+                <br/>
 
-                <div class="row container mx-3" style={{borderRadius: "25px"}}>
+                <div class="row card-body mx-3" style={{borderRadius: "25px"}}>
 
-                    <div class="col-md-9 mb-md-0 mb-5">
+                    <div class="col-md-9 mb-md-0 mb-5 text-center mx-auto">
                         <form id="contact-form" name="contact-form" action="mail.php" method="POST">
 
                             <div class="row">
@@ -28,14 +30,14 @@ const FormularioCont = () => {
                                 <div class="col-md-6">
                                     <div class="md-form mb-0">
                                         <input type="text" id="name" name="name" class="form-control"/>
-                                        <label for="name" class="">Your name</label>
+                                        <label for="name" class="">Nombre</label>
                                     </div>
                                 </div>
 
                                 <div class="col-md-6">
                                     <div class="md-form mb-0">
                                         <input type="text" id="email" name="email" class="form-control"/>
-                                        <label for="email" class="">Your email</label>
+                                        <label for="email" class="">Email</label>
                                     </div>
                                 </div>
 
@@ -45,7 +47,7 @@ const FormularioCont = () => {
                                 <div class="col-md-12">
                                     <div class="md-form mb-0">
                                         <input type="text" id="subject" name="subject" class="form-control"/>
-                                        <label for="subject" class="">Subject</label>
+                                        <label for="subject" class="">Asunto</label>
                                     </div>
                                 </div>
                             </div>
@@ -56,7 +58,7 @@ const FormularioCont = () => {
 
                                     <div class="md-form">
                                         <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
-                                        <label for="message">Your message</label>
+                                        <label for="message">Mensaje</label>
                                     </div>
 
                                 </div>
@@ -64,35 +66,46 @@ const FormularioCont = () => {
 
                         </form>
 
-                        <div class="text-center text-md-left">
-                            <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+                        <div class="text-center col-md-12 ">
+                            <button type="button" className="btn btn-outline-primary btn-rounded waves-effect btn-lg btn-block ">Enviar</button>
+                            <br/>
+                            <br/>
                         </div>
-                        <div class="status"></div>
                     </div>
+                    <hr width="95%"/>
 
-                    <div class="col-md-3 text-center">
-                        <ul class="list-unstyled mb-0">
-                            <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                    <div class="col-md-12 text-center ">
+                        <ul class="list-unstyled mt-2 mb-2">
+                            <br/>
+                            <br/>
+                            <li><i class="fas fa-map-marker-alt fa-2x blue-text"></i>
                                 <p>MARCELO T. DE ALVEAR 145 - PISO 9 - OFICINA 8 CIUDAD DE RESISTENCIA.</p>
                             </li>
 
-                            <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                            <li><i class="fas fa-phone mt-4 fa-2x blue-text"></i>
                                 <p>TEL: 4448089</p>
                             </li>
 
-                            <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                            <li><i class="fas fa-envelope mt-4 fa-2x blue-text"></i>
                                 <p>EDUCACIONAMBIENTALCHACO@GMAIL.COM</p>
                             </li>
+                            <br/>
+                            <br/>
                         </ul>
+                    </div>
+
+                    <br/>
+                    <br/>
+                    <br/>
+
+                    <div className="col-md-12 mt-3" style={{borderRadius: "25px"}}>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3540.633423214561!2d-58.98951798494358!3d-27.44953288289944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94450cf504e7b2eb%3A0x62033b0a800da69!2sPISO%209%20-%20OFICINA%208%2C%20Marcelo%20T.%20de%20Alvear%20145%2C%20Resistencia%2C%20Chaco!5e0!3m2!1ses!2sar!4v1573549735767!5m2!1ses!2sar"  height="350" frameborder="0" style={{border:"0",width:"100%",margin:"auto"}} allowfullscreen=""></iframe>
                     </div>
 
                 </div>
 
-            </section>
-            <div className="container-fluid mapa">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3540.633423214561!2d-58.98951798494358!3d-27.44953288289944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94450cf504e7b2eb%3A0x62033b0a800da69!2sPISO%209%20-%20OFICINA%208%2C%20Marcelo%20T.%20de%20Alvear%20145%2C%20Resistencia%2C%20Chaco!5e0!3m2!1ses!2sar!4v1573549735767!5m2!1ses!2sar"  height="350" frameborder="0" style={{border:"0",width:"80vw",margin:"auto"}} allowfullscreen=""></iframe>
             </div>
-            <Footer/>
+            
 
         </React.Fragment>
     )
--- educacion-ambiental-chaco/src/Componentes/getGaleria.js
+++ educacion-ambiental-chaco/src/Componentes/getGaleria.js
@@ -7,6 +7,7 @@ import './css/getGaleria.css'
 
 
 
+
 function GetGaleria({filtro}){
     const [imagenes,guardarImagenes]=useState([]);
     const[paginaActual,guardarPaginaActual]=useState(1);
@@ -67,3 +68,115 @@ function GetGaleria({filtro}){
     )
 }
 export default GetGaleria
+/*
+
+
+
+function GetGaleria({filtro}){
+    const [imagenes,guardarImagenes]=useState([]);
+    const[paginaActual,guardarPaginaActual]=useState(1);
+    const [totalPaginas,guardarTotalPaginas]=useState(1);
+    const [contador,guardarcontador]=useState(0)
+
+    useEffect(() => {
+      const consultarApi = async () =>{
+          const url= `https://localhost:44398/api/Contenido/GetAll`;
+         
+          const respuesta = await fetch(url
+          );
+          const resultado = await respuesta.json();
+          guardarImagenes(resultado.data)
+          guardarTotalPaginas(4)
+          const calculatTotalPaginas=Math.ceil(imagenes.length/30);
+          guardarTotalPaginas(calculatTotalPaginas)
+          
+
+      }
+      consultarApi();
+    },[imagenes,paginaActual])
+
+    const paginaAnterior = () =>{
+        let nuevaPaginaActual = paginaActual -1;
+        guardarPaginaActual(nuevaPaginaActual);
+
+    }
+
+    const paginaSiguiente = () =>{
+        let nuevaPaginaActual = paginaActual+1;
+        guardarPaginaActual(nuevaPaginaActual)
+        let nuevocont = contador+1
+        guardarcontador(nuevocont)
+    }
+
+
+    const pedazo = imagenes.slice(imagenes[0],[16])
+    const result = filtro;
+
+    let mostrar;
+
+    switch (contador) {
+        case 0:
+          mostrar=imagenes.slice(imagenes[0],[16])
+          break;
+        case 1:
+            mostrar=imagenes.slice(imagenes[17],[33])
+          break;
+        case 2:
+            mostrar=imagenes.slice(imagenes[34],[50])
+            break;
+        case 3:
+            mostrar=imagenes.slice(imagenes[51],[67])
+            break;
+        case 4:
+            mostrar=imagenes.slice(imagenes[68],[84])
+            break;
+        case 5:
+            mostrar=imagenes.slice(imagenes[85],[101])
+            break;
+      
+        default:
+            mostrar=imagenes
+          break;
+      }
+      
+
+    if(result!==''){
+        //mostrar=pedazo.filter(user => user.categoria==result)
+    } else{
+        //mostrar=imagenes
+        console.log(imagenes)
+        console.log(imagenes.length)
+        console.log(imagenes[0])
+       
+    }
+
+    
+
+    
+    
+    return(
+
+        <div fluid className="container-fluid">
+            <div className="text-center">
+
+                <Galeria
+                    imagenes={mostrar}
+                    categoria={filtro}
+                />
+                {(paginaActual===1) ? null :(
+                    <button onClick={paginaAnterior} type="button" className="btn btn-info mr-1">Anterior &laquo;</button>
+                )}
+
+                {(paginaActual===totalPaginas) ? null:(
+                    <button onClick={paginaSiguiente} type="button" className="btn btn-info mr-1">Siguiente &raquo;</button>                    
+                )}
+
+                <button className="btn btn-info" type="button" onClick={paginaSiguiente} >next</button>
+            </div>
+        </div> 
+
+    )
+}
+export default GetGaleria
+
+*/
